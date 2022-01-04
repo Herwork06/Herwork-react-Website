b0b2d739023a61636dfbaf2164aabc34e80a1647
Herwork06 <herwork06@gmail.com>
2022-01-04 23:37:36 +0100

    'BoxUI'

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+      <div class="filter">
       <header className="App-header">
-        <div class="center font">
+      <div class="box">
+      <div class="center font">
+        
+        
     <h1>Herwork06</h1>
     <p>Hi! I am Herwork06 a coder, 3D artist and linux server developer who lives in Norway 🇳🇴 </p>
     <p>Links: </p>
@@ -20,11 +25,13 @@ function App() {
     <br></br>
     <a href="https://www.youtube.com/channel/UC6cs1e1HpujA4yNGAdUYfMg" target="_blank" rel="noreferrer" class='link'>YouTube</a>
     
+    </div>
 </div>
       </header>
       <footer class='font'>
         <a href="https://www.reddit.com/r/wallpaper/comments/rkf90a/colorful_mountains_1920x1080/" target="_blank" rel="noreferrer" class='link'>Image Source</a>
     </footer>
+    </div>
     </div>
   );
 }
