f303cf163e64fdd8927185b2ec2974d122e32615
Herwork06 <herwork06@gmail.com>
2022-01-04 22:48:33 +0100

    'WebsiteUI'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       Deployment is happening here!
+        <div class="center font">
+    <h1>Herwork06</h1>
+    <p>Hi! I am Herwork06 a coder, 3D artist and linux server developer who lives in Norway 🇳🇴 </p>
+    <p>Links: </p>
+    <a href="404.html" class='link'>Projects</a>
+    <br></br>
+    <a href="404.html" target="_blank" rel="noreferrer" class='link'>Discord</a>
+    <br></br>
+    <a href="https://twitter.com/Herwork06" target="_blank" rel="noreferrer" class='link'>Twitter</a>
+    <br></br>
+    <a href="https://www.twitch.tv/herwork06" target="_blank" rel="noreferrer" class='link'>Twitch</a>
+    <br></br>
+    <a href="https://www.youtube.com/channel/UC6cs1e1HpujA4yNGAdUYfMg" target="_blank" rel="noreferrer" class='link'>YouTube</a>
+    
+</div>
       </header>
+      <footer class='font'>
+        <a href="https://www.reddit.com/r/wallpaper/comments/rkf90a/colorful_mountains_1920x1080/" target="_blank" rel="noreferrer" class='link'>Image Source</a>
+    </footer>
     </div>
   );
 }
